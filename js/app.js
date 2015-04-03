9706ee6b23ee297f2a8845f67c46281e72835b54
Raynor <damaskin@live.ru>
2015-04-03 17:49:28 +0300

    BlackList App

--- js/app.js
+++ js/app.js
@@ -1,3 +1,34 @@
 /**
- * Created by Ivan on 02.04.2015.
+ * Created by Ivan on 03.04.2015.
  */
+
+var BlackList = angular.module('BlackList', [
+    'ngRoute'
+]);
+
+BlackList.config(['$routeProvider', '$locationProvider',
+    function($routeProvider, $locationProvider) {
+        $locationProvider.html5Mode({
+                enabled: true,
+                requirebase: false
+            }
+        );
+
+        $routeProvider.
+            when('/', {
+                templateUrl: 'pages/home.html',
+                controller: 'BlackListCtrl'
+            }).
+            when('/about', {
+                templateUrl: 'pages/about.html',
+                controller: 'AboutCtrl'
+            }).
+            when('/contact', {
+                templateUrl: 'pages/contact.html',
+                controller: 'ContactCtrl'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+    }]);
+
---
+++ js/controllers.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Ivan on 03.04.2015.
+ */
+
+BlackList.controller('BlackListCtrl', ['$scope', '$http', function($scope, $http){
+    $scope.name = 'Home';
+
+    $http.get('/objects/blacklist.json').success(function(data){
+        $scope.blacklist = data;
+    })
+}]);
+
+BlackList.controller('AboutCtrl', ['$scope', '$http', function($scope, $http){
+    $scope.name = 'About';
+}]);
+
+BlackList.controller('ContactCtrl', ['$scope', '$http', function($scope, $http){
+    $scope.name = 'Contact';
+}]);
